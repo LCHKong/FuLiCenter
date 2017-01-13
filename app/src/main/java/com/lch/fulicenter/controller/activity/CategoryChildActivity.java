ed08343175fcbf44c2fd6a279164c565a8ef25ac
LCH <765353128@qq.com>
2017-01-13 19:41:09 +0800

    分类二级页面的排序以及箭头方向

--- app/src/main/java/com/lch/fulicenter/controller/activity/CategoryChildActivity.java
+++ app/src/main/java/com/lch/fulicenter/controller/activity/CategoryChildActivity.java
@@ -1,8 +1,10 @@
 package com.lch.fulicenter.controller.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.lch.fulicenter.R;
@@ -16,8 +18,15 @@ import butterknife.OnClick;
 
 public class CategoryChildActivity extends AppCompatActivity {
     NewGoodsFragment mNewGoodsFragment;
+    boolean priceAsc = false;
+    boolean addTimeAsc = false;
+
     @BindView(R.id.tv_common_title)
     TextView tvCommonTitle;
+    @BindView(R.id.btnPriceSort)
+    Button btnPriceSort;
+    @BindView(R.id.btnAddTimeSort)
+    Button btnAddTimeSort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +44,35 @@ public class CategoryChildActivity extends AppCompatActivity {
 
     @OnClick({R.id.btnPriceSort, R.id.btnAddTimeSort})
     public void onClick(View view) {
+        int sortBy = I.SORT_BY_PRICE_ASC;
+        Drawable right;
         switch (view.getId()) {
             case R.id.btnPriceSort:
-                mNewGoodsFragment.sortGoods(I.SORT_BY_PRICE_ASC);
+                if (priceAsc) {
+                    sortBy = I.SORT_BY_PRICE_ASC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
+                } else {
+                    sortBy = I.SORT_BY_PRICE_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
+                }
+                right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
+                btnPriceSort.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, right, null);
+                priceAsc = !priceAsc;
                 break;
             case R.id.btnAddTimeSort:
-                mNewGoodsFragment.sortGoods(I.SORT_BY_ADDTIME_ASC);
+                if (addTimeAsc) {
+                    sortBy = I.SORT_BY_ADDTIME_ASC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_up);
+                } else {
+                    sortBy = I.SORT_BY_ADDTIME_DESC;
+                    right = getResources().getDrawable(R.mipmap.arrow_order_down);
+                }
+                right.setBounds(0, 0, right.getIntrinsicWidth(), right.getIntrinsicHeight());
+                btnAddTimeSort.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, right, null);
+                addTimeAsc = !addTimeAsc;
                 break;
         }
+        mNewGoodsFragment.sortGoods(sortBy);
     }
 
     @OnClick(R.id.ivBack)
