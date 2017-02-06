e49c29eb586bd20e9b8ac10e44c5d463ff6b0c42
LCH <765353128@qq.com>
2017-02-06 11:31:48 +0800

    收货地址的非空验证

--- app/src/main/java/com/lch/fulicenter/controller/activity/OrderActivity.java
+++ app/src/main/java/com/lch/fulicenter/controller/activity/OrderActivity.java
@@ -2,9 +2,11 @@ package com.lch.fulicenter.controller.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lch.fulicenter.R;
 import com.lch.fulicenter.application.I;
@@ -43,5 +45,37 @@ public class OrderActivity extends AppCompatActivity {
     @OnClick(R.id.ivBack)
     public void onClick() {
         MFGT.finish(this);
+    }
+
+    @OnClick(R.id.btnBuy)
+    public void checkOrder() {
+        String receiveName = metReceiverName.getText().toString();
+        if (TextUtils.isEmpty(receiveName)) {
+            metReceiverName.setError("收货人姓名不能为空");
+            metReceiverName.requestFocus();
+            return;
+        }
+        String phone = metPhone.getText().toString();
+        if (TextUtils.isEmpty(phone)) {
+            metPhone.setError("手机号码不能为空");
+            metPhone.requestFocus();
+            return;
+        }
+        if (!phone.matches("[\\d]{11}")) {
+            metPhone.setError("手机号码格式错误");
+            metPhone.requestFocus();
+            return;
+        }
+        String area = mspinArea.getSelectedItem().toString();
+        if (TextUtils.isEmpty(area)) {
+            Toast.makeText(this, "收货地区不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String address = metAddress.getText().toString();
+        if (TextUtils.isEmpty(address)) {
+            metAddress.setError("街道地址不能为空");
+            metAddress.requestFocus();
+            return;
+        }
     }
 }
